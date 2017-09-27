c005623fcdf4b8830f4378f2705ddcdebe1a52e3
Ahmed Abdel Aal <thelastrealdevil@gmail.com>
2017-09-28 00:48:24 +0200

    Replace Fixed test with commnad OpenGL_Version_Test

--- test/OpenGLVersionTest.cpp
+++ test/OpenGLVersionTest.cpp
@@ -1,7 +1,9 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "DebugInfo.hpp"
+#include "testutility.hpp"
 
 TEST_CASE("OpenGL Version", "[OpenGLVersion]") {
-    REQUIRE(SystemInfo::getOpenGL_Version() == "4.5.0 NVIDIA 384.59");
+  auto OpenGL_Version = getCommand("glxinfo -B | awk -F: '/OpenGL version string/{ sub(/^[ ]+/, \"\", $2); print $2 }'");
+  REQUIRE(SystemInfo::getOpenGL_Version() == OpenGL_Version);
 }
