6bcfd8b073ae3383bf4b160715984e28e2af679b
Ahmed Abdel Aal <thelastrealdevil@gmail.com>
2017-09-28 00:48:06 +0200

    Replace Fixed test with commnad CPU_Test

--- test/CpuTest.cpp
+++ test/CpuTest.cpp
@@ -1,17 +1,21 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "DebugInfo.hpp"
+#include "testutility.hpp"
 
 TEST_CASE("CPU Model", "[CpuModel]") {
-    REQUIRE(SystemInfo::getCPU_Name() == "Intel(R) Core(TM) i7-6700HQ CPU @ 2.60GHz");
+  auto CPU_Name = getCommand("awk -F: '/model name/ { sub(/^[ ]+/, \"\", $2); print $2; exit }' /proc/cpuinfo");
+  REQUIRE(SystemInfo::getCPU_Name() == CPU_Name);
 }
 
 TEST_CASE("CPU Vendor", "[CpuVendorId]") {
-    REQUIRE(SystemInfo::getCPU_Vendor() == "GenuineIntel");
+  auto CPU_Vendor = getCommand("awk -F: '/vendor_id/ { sub(/^[ ]+/, \"\", $2); print $2; exit }' /proc/cpuinfo");
+  REQUIRE(SystemInfo::getCPU_Vendor() == CPU_Vendor);
 }
 
 TEST_CASE("CPU Cache", "[CpuCache]") {
-    REQUIRE(SystemInfo::getCPU_Cache() == "6144 KB");
+  auto CPU_Cache = getCommand("awk -F: '/cache/ { sub(/^[ ]+/, \"\", $2); print $2; exit }' /proc/cpuinfo");
+  REQUIRE(SystemInfo::getCPU_Cache() == CPU_Cache);
 }
 
 TEST_CASE("CPU Cores", "[CpuCores]") {
