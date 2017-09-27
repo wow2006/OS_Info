5661ea33af11efa2ff3dbb9b5487984528cabd61
Ahmed Abdel Aal <thelastrealdevil@gmail.com>
2017-09-28 00:53:50 +0200

    Replace Fixed test with commnad KernelTest

--- test/KernelTest.cpp
+++ test/KernelTest.cpp
@@ -1,15 +1,19 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "DebugInfo.hpp"
+#include "testutility.hpp"
 
 TEST_CASE("Kernel Name", "[KernelName]") {
-    REQUIRE(SystemInfo::getKernel_Name() == "Linux");
+  auto kernelName = getCommand("uname");
+  REQUIRE(SystemInfo::getKernel_Name() == kernelName);
 }
 
 TEST_CASE("CPU Arch", "[CPU_Arch]") {
-    REQUIRE(SystemInfo::getKernel_Arch() == "x86_64");
+  auto arch = getCommand("uname -p");
+  REQUIRE(SystemInfo::getKernel_Arch() == arch);
 }
 
 TEST_CASE("Kernel Release", "[KernelRelease]") {
-    REQUIRE(SystemInfo::getKernel_Release() == "4.10.0-33-generic");
+  auto release = getCommand("uname -r");
+  REQUIRE(SystemInfo::getKernel_Release() == release);
 }
