b08ebc70f8e8ffe503a1fd1390a80af2138a13b6
Ahmed Abdel Aal <thelastrealdevil@gmail.com>
2017-10-07 00:43:53 +0200

    Fix test

--- src/linux/glibcInfo.cpp
+++ src/linux/glibcInfo.cpp
@@ -1,15 +1,9 @@
 #include "linux/glibcInfo.hpp"
-#ifdef GCC_VERSION
 #include <gnu/libc-version.h>
-#endif
 
 using map = GlibcInfo::map;
 
 bool GlibcInfo::read_glibc(map& info) {
-#ifdef GCC_VERSION
   info["version"] = gnu_get_libc_version();
-#else
-  info["version"] = "Unknow"
-#endif
   return !info["version"].empty();
 }
